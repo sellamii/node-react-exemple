dce9818ef774156d1e7ace9e56b62fd148abc9ec
sellamii <33124276+sellamii@users.noreply.github.com>
2020-01-23 16:51:29 +0100

    Create getusers.js

---
+++ backend/src/test/getusers.js
@@ -0,0 +1,32 @@
+
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../app');
+//let should = chai.should();
+
+
+chai.use(chaiHttp);
+//Our parent block
+describe('users', () => {
+    beforeEach((done) => { //Before each test we empty the database
+        User.remove({}, (err) => { 
+           done();           
+        });        
+    });
+/*
+  * Test the /GET route
+  */
+  describe('/GET users', () => {
+      it('it should GET all the users', (done) => {
+        chai.request(server)
+            .get('/users')
+            .end((err, res) => {
+                  res.should.have.status(200);
+                  res.body.should.be.a('array');
+                  res.body.length.should.be.eql(0);
+              done();
+            });
+      });
+  });
+
+});
