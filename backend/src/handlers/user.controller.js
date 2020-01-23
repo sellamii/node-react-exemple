2207b4d68e354b730c22f31734344a29103466c2
sellamii <33124276+sellamii@users.noreply.github.com>
2020-01-23 16:49:39 +0100

    Create user.controller.js

---
+++ backend/src/handlers/user.controller.js
@@ -0,0 +1,62 @@
+
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
+  password: 'password',
+  port: 5432,
+});
+
+const { GET , GETID ,POST ,PUT , DELETE} = require ('../database/db');
+
+
+const getUsers = async (req, res) => {
+    const response = await pool.query(GET);
+    res.status(200).json(response.rows);
+};
+
+const getUserById = async (req, res) => {
+    const id = parseInt(req.params.id);
+    const response = await pool.query(GETID , [id]);
+    res.json(response.rows);
+};
+
+const createUser = async (req, res) => {
+    const { name, email } = req.body;
+    const response = await pool.query(POST, [name, email]);
+    res.json({
+        message: 'User Added successfully',
+        body: {
+            user: {name, email}
+        }
+    })
+};
+
+const updateUser = async (req, res) => {
+    const id = parseInt(req.params.id);
+    const { name, email } = req.body;
+
+    const response =await pool.query(PUT, [
+        name,
+        email,
+        id
+    ]);
+    res.json('User Updated Successfully');
+};
+
+const deleteUser = async (req, res) => {
+    const id = parseInt(req.params.id);
+    await pool.query(DELETE, [
+        id
+    ]);
+    res.json(`User ${id} deleted Successfully`);
+};
+
+module.exports = {
+    getUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
+};
